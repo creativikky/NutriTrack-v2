06734b43adbe81332b1b438e4549318d02dfab2a
creativikky <creativikky@gmail.com>
2025-05-14 21:35:02 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// js/app.js
+import { auth } from './firebase-init.js';
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-auth.js";
+
+export function redirectIfNotLoggedIn() {
+  onAuthStateChanged(auth, user => {
+    if (!user) window.location = 'index.html';
+  });
+}
+
+export function logout() {
+  signOut(auth).then(() => window.location = 'index.html');
+}
